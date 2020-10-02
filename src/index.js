2f9bb6d14480be38265e30089710410c643b9ad1
Nikita Rastorguev <nirastor@gmail.com>
2020-10-02 17:40:27 +0300

    index.js clear

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-/* eslint-disable no-console */
 import './css/style.css';
-
-import Settings from './js/settings';
-
-const S = new Settings();
-
-console.log(S);
-console.log(S.setting);
-console.log(S.setting.get('theme'));
